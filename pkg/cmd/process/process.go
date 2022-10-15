2d6b6c9e5354e8562ce7edc1d140575e35f6ec94
Marco Braga <mrbraga@redhat.com>
2022-10-14 21:58:52 -0300

    cmd/process: split result on the pkg

--- pkg/cmd/process/process.go
+++ pkg/cmd/process/process.go
@@ -1,64 +1,21 @@
 package process
 
 import (
-	"compress/gzip"
 	"fmt"
 	"os"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
-	"github.com/vmware-tanzu/sonobuoy/pkg/client/results"
-	"github.com/vmware-tanzu/sonobuoy/pkg/discovery"
 	"github.com/vmware-tanzu/sonobuoy/pkg/errlog"
 	"text/tabwriter"
 )
 
-const (
-	// resultModeReport prints a human-readable summary of the results to stdout.
-	resultModeReport = "report"
-
-	// resultModeDetailed will dump each leaf node (e.g. test) as a json object. If the results
-	// are just references to files (like systemd-logs) then it will print the file for each
-	// leaf node, prefixed with the path.
-	resultModeDetailed = "detailed"
-
-	// resultModeDump will just copy the post-processed yaml file to stdout.
-	resultModeDump = "dump"
-
-	//resultModeReadable will copy the post-processed yaml file to stdout and replace \n and \t with new lines and tabs respectively.
-	resultModeReadable = "readable"
-
-	windowsSeperator = `\`
-
-	//Name of the "fake" plugin used to enable printing the health summary.
-	//This name needs to be reserved to avoid conflicts with a plugin with the same name
-	clusterHealthSummaryPluginName = "sonobuoy"
-
-	// OpenShift Custom Resources
-	openshiftCrInfrastructureFilePath = "resources/cluster/config.openshift.io_v1_infrastructures.json"
-	openshiftCrCvoPath                = "resources/cluster/config.openshift.io_v1_clusterversions.json"
-	openshiftCrCoPath                 = "resources/cluster/config.openshift.io_v1_clusteroperators.json"
-)
-
-type resultsInput struct {
+type Input struct {
 	archive     string
 	archiveBase string
-	plugin      string
 	suiteOCP    string
 	suiteKube   string
-	mode        string
-	node        string
-	skipPrefix  bool
-}
-
-// ResultSummary holds the summary of a single execution
-type ResultSummary struct {
-	name      string
-	archive   string
-	cluster   discovery.ClusterSummary
-	openshift *OpenShiftSummary
-	input     *resultsInput
 }
 
 // ConsolidatedSummary Aggregate the results of provider and baseline
@@ -69,13 +26,13 @@ type ConsolidatedSummary struct {
 }
 
 func NewCmdProcess() *cobra.Command {
-	data := resultsInput{}
+	data := Input{}
 	cmd := &cobra.Command{
 		Use:   "process archive.tar.gz",
 		Short: "Inspect plugin results.",
 		Run: func(cmd *cobra.Command, args []string) {
 			data.archive = args[0]
-			if err := processResult(data); err != nil {
+			if err := processResult(&data); err != nil {
 				errlog.LogError(errors.Wrapf(err, "could not process archive: %v", args[0]))
 				os.Exit(1)
 			}
@@ -101,43 +58,18 @@ func NewCmdProcess() *cobra.Command {
 	return cmd
 }
 
-// getReader returns a *results.Reader along with a cleanup function to close the
-// underlying readers. The cleanup function is guaranteed to never be nil.
-func getReader(filepath string) (*results.Reader, func(), error) {
-	fi, err := os.Stat(filepath)
-	if err != nil {
-		return nil, func() {}, err
-	}
-	if fi.IsDir() {
-		return results.NewReaderFromDir(filepath), func() {}, nil
-	}
-	f, err := os.Open(filepath)
-	if err != nil {
-		return nil, func() {}, errors.Wrapf(err, "could not open sonobuoy archive: %v", filepath)
-	}
 
-	gzr, err := gzip.NewReader(f)
-	if err != nil {
-		return nil, func() { f.Close() }, errors.Wrap(err, "could not make a gzip reader")
-	}
-
-	r := results.NewReaderWithVersion(gzr, results.VersionTen)
-	return r, func() { gzr.Close(); f.Close() }, nil
-}
-
-func processResult(input resultsInput) error {
+func processResult(input *Input) error {
 
 	cs := ConsolidatedSummary{
 		provider: &ResultSummary{
 			name:      "provider",
 			archive:   input.archive,
-			input:     &input,
 			openshift: NewOpenShiftSummary(),
 		},
 		baseline: &ResultSummary{
 			name:      "base",
 			archive:   input.archiveBase,
-			input:     &input,
 			openshift: NewOpenShiftSummary(),
 		},
 		suites: &openshiftTestsSuites{
@@ -183,132 +115,7 @@ func processResult(input resultsInput) error {
 	return err
 }
 
-func populateResult(rs *ResultSummary) error {
 
-	reader, cleanup, err := getReader(rs.archive)
-	defer cleanup()
-	if err != nil {
-		return err
-	}
-
-	// Report on all plugins or the specified one.
-	plugins, err := getPluginList(reader)
-	if err != nil {
-		return errors.Wrapf(err, "unable to determine plugins to report on")
-	}
-	if len(plugins) == 0 {
-		return fmt.Errorf("no plugins specified by either the --plugin flag or tarball metadata")
-	}
-
-	var lastErr error
-	for _, plugin := range plugins {
-		err := processPlugin(rs, plugin)
-		if err != nil {
-			lastErr = err
-		}
-	}
-
-	reader, cleanup, err = getReader(rs.archive)
-	defer cleanup()
-	if err != nil {
-		lastErr = err
-	}
-
-	err = populateSummary(reader, rs)
-	if err != nil {
-		lastErr = err
-	}
-
-	return lastErr
-}
-
-func processPlugin(rs *ResultSummary, plugin string) error {
-	reader, cleanup, err := getReader(rs.archive)
-	defer cleanup()
-	if err != nil {
-		return err
-	}
-
-	obj, err := reader.PluginResultsItem(plugin)
-	if err != nil {
-		return err
-	}
-
-	err = processPluginResult(obj, rs)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func processPluginResult(obj *results.Item, rs *ResultSummary) error {
-	statusCounts := map[string]int{}
-	var failedList []string
-
-	statusCounts, failedList = walkForSummary(obj, statusCounts, failedList)
-
-	total := 0
-	for _, v := range statusCounts {
-		total += v
-	}
-
-	rs.openshift.setPluginResult(&OPCTPluginSummary{
-		Name:       obj.Name,
-		Status:     obj.Status,
-		Total:      int64(total),
-		Passed:     int64(statusCounts[results.StatusPassed]),
-		Failed:     int64(statusCounts[results.StatusFailed] + statusCounts[results.StatusTimeout]),
-		Timeout:    int64(statusCounts[results.StatusTimeout]),
-		Skipped:    int64(statusCounts[results.StatusSkipped]),
-		FailedList: failedList,
-	})
-
-	delete(statusCounts, results.StatusPassed)
-	delete(statusCounts, results.StatusFailed)
-	delete(statusCounts, results.StatusTimeout)
-	delete(statusCounts, results.StatusSkipped)
-
-	return nil
-}
-
-// printHealthSummary pretends to work like printSinglePlugin
-// but for a "fake" plugin that prints health information
-func populateSummary(r *results.Reader, rs *ResultSummary) error {
-
-	ocpInfra := OpenShiftCrInfrastructures{}
-	ocpCVO := OpenShiftCrCvo{}
-	ocpCO := OpenShiftCrCo{}
-
-	// For summary and dump views, get the item as an object to iterate over.
-	err := r.WalkFiles(func(path string, info os.FileInfo, err error) error {
-		err = results.ExtractFileIntoStruct(results.ClusterHealthFilePath(), path, info, &rs.cluster)
-		if err != nil {
-			return err
-		}
-		err = results.ExtractFileIntoStruct(openshiftCrInfrastructureFilePath, path, info, &ocpInfra)
-		if err != nil {
-			return err
-		}
-		err = results.ExtractFileIntoStruct(openshiftCrCvoPath, path, info, &ocpCVO)
-		if err != nil {
-			return err
-		}
-		err = results.ExtractFileIntoStruct(openshiftCrCoPath, path, info, &ocpCO)
-		if err != nil {
-			return err
-		}
-		return err
-	})
-	if err != nil {
-		return err
-	}
-
-	rs.openshift.setFromInfraCR(&ocpInfra)
-	rs.openshift.setFromCvoCR(&ocpCVO)
-	rs.openshift.setFromCoCR(&ocpCO)
-
-	return nil
-}
 
 func printAggregatedTable(cs *ConsolidatedSummary) error {
 	fmt.Printf("\n> OpenShift Provider Certification Summary <\n\n")
@@ -392,30 +199,4 @@ func printAggregatedTable(cs *ConsolidatedSummary) error {
 	tbWriter.Flush()
 
 	return nil
-}
-
-func getPluginList(r *results.Reader) ([]string, error) {
-	runInfo := discovery.RunInfo{}
-	err := r.WalkFiles(func(path string, info os.FileInfo, err error) error {
-		return results.ExtractFileIntoStruct(r.RunInfoFile(), path, info, &runInfo)
-	})
-
-	return runInfo.LoadedPlugins, errors.Wrap(err, "finding plugin list")
-}
-
-func walkForSummary(result *results.Item, statusCounts map[string]int, failList []string) (map[string]int, []string) {
-	if len(result.Items) > 0 {
-		for _, item := range result.Items {
-			statusCounts, failList = walkForSummary(&item, statusCounts, failList)
-		}
-		return statusCounts, failList
-	}
-
-	statusCounts[result.Status]++
-
-	if result.Status == results.StatusFailed || result.Status == results.StatusTimeout {
-		failList = append(failList, result.Name)
-	}
-
-	return statusCounts, failList
 }
